29e9e2e2f3a0640fbb98fcf4cd6f4656cb373c44
Jahinur160871 <138611921+Jahinur160871@users.noreply.github.com>
2026-06-17 22:25:50 +0600

    Delete Final directory

--- Final/backend/middleware/seller.js
+++
@@ -1,9 +0,0 @@
-const isApprovedSeller = (req, res, next) => {
-  if (req.user && req.user.isSeller && req.user.sellerApproved) {
-    next();
-  } else {
-    res.status(403).json({ message: 'Not authorized as approved seller' });
-  }
-};
-
-export { isApprovedSeller };
